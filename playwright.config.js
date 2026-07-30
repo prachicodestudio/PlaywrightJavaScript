5dd6ba745129d2ea65600618add6c6aa5b15ab0d
prachicodestudio <prachicodechannel@gmail.com>
2026-07-30 17:58:03 +0530

    session 34

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,9 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'line',
   //reporter: [['json', { outputFile: 'results.json' }]],
-   reporter: [['junit', { outputFile: 'results.xml' }]],
+   //reporter: [['junit', { outputFile: 'results.xml' }]],
+    reporter: [['line'],['allure-playwright']],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
